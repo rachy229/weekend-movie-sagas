e5c9c7c95ee65f9efbeed63967beab5759852718
Rachel Bruce <rachelbruce922@gmail.com>
2022-07-23 12:25:41 -0500

    created router.get in genre.router

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,16 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
+  const queryText = `SELECT * FROM "genres" 
+  JOIN "movies_genres" ON "genres"."id" = "movies_genres"."genre_id" 
+  JOIN "movies" ON "movies"."id" = "movies_genres"."movie_id";`;
+  pool.query(queryText)
+  .then(result => {
+    res.send(result.rows);
+  }).catch(error => {
+    console.log('error in genre.router GET', error)
   res.sendStatus(500)
+  })
 });
 
 module.exports = router;
